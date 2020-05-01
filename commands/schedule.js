6254e457e896dd7cb8a426cdb718ba6555601378
Riley McGinn <rileysmcginn@gmail.com>
2020-05-01 02:02:21 -0700

    moved callbacks outside of module.exports

--- commands/schedule.js
+++ commands/schedule.js
@@ -2,20 +2,18 @@ const path = require('path');
 var FileInterface = require(path.join(process.cwd(), 'fileInterface.js'));
 const fileInterface = new FileInterface();
 
+var callback = function(res) {
+    client.say(target, res);
+}
+
+var failCallback = function(err) {
+    console.log(`* Critical error with !schedule, disabling command.`);
+    console.log(`  * ` + err);
+    commandConfig.setEnabled("!schedule", false);
+}
+
 module.exports = {
     executeCommand: function(client, target, context, parameters, commandConfig) {
-        
-        // Can I move these callbacks outside of the export?
-        var callback = function(res) {
-            client.say(target, res);
-        }
-
-        var failCallback = function(err) {
-            console.log(`* Critical error with !schedule, disabling command.`);
-            console.log(`  * ` + err);
-            commandConfig.setEnabled("!schedule", false);
-        }
-
         fileInterface.readFromFile('schedule.txt', callback, failCallback);
     }
 }
--- commands/setschedule.js
+++ commands/setschedule.js
@@ -2,15 +2,14 @@ const path = require('path');
 var FileInterface = require(path.join(process.cwd(), 'fileInterface.js'));
 const fileInterface = new FileInterface();
 
+var failCallback = function(err) {
+    console.log(`* Critical error with !setschedule, disabling command.`);
+    console.log(`  * ` + err);
+    commandConfig.setEnabled("!setschedule", false);
+}
 
 module.exports = {
     executeCommand: function(client, target, context, parameters, commandConfig) {
-        var failCallback = function(err) {
-            console.log(`* Critical error with !setschedule, disabling command.`);
-            console.log(`  * ` + err);
-            commandConfig.setEnabled("!setschedule", false);
-        }
-        
         console.log(parameters);
         fileInterface.writeToFile(('schedule.txt'), parameters[0], failCallback);
     }
